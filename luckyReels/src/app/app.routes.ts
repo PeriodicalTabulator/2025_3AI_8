1f171a6f77fd62f4d019fceef5ff56e705245e67
BacmanXD <samuelbacka71@gamil.com>
2025-02-12 08:22:54 +0100

    Main-Page

--- luckyReels/src/app/app.routes.ts
+++ luckyReels/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MenuComponent },
+  { path: 'Hra1', component: MenuComponent },
+  { path: 'Hra2', component: MenuComponent },
+  { path: 'Hra3', component: MenuComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
