72864f0b01ace4516f3f6fb79b130bbaa06c154c
asdf <romanfholes@gmail.com>
2025-03-11 15:13:49 +0100

    slots ui changes

--- luckyReels/src/app/app.routes.ts
+++ luckyReels/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { SlotsComponent } from '../slots/slots.component';
 import { BeancanComponent } from '../beancan/beancan.component';
 import { BlackjackComponent } from '../blackjack/blackjack.component';
 import { MenuComponent } from '../menu/menu.component';
+import { LoginComponent } from '../login/login.component';
 
 export const routes: Routes = [
   { path: 'slots', component: SlotsComponent },
   { path: 'beancan', component:BeancanComponent },
   {path: 'blackjack', component: BlackjackComponent},
-  {path: '', component:MenuComponent}
+  {path: '', component:MenuComponent},
+  {path: 'login', component:LoginComponent}
 ];
 
 @NgModule({
