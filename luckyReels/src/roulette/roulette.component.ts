d1cb724898f2b9e591635e7c0f109c5ff886e5c2
asdf <romanfholes@gmail.com>
2025-03-26 09:50:07 +0100

    roulette bet table

--- luckyReels/src/roulette/roulette.component.ts
+++ luckyReels/src/roulette/roulette.component.ts
@@ -15,6 +15,17 @@ export class RouletteComponent {
   isSpinning = false;
   spinDuration = 5000;
 
+  selectedChip = 1;
+  currentBetAmount = 0;
+  bets: {number: number | string, amount: number}[] = [];
+  
+  numberRows = [
+    [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36],
+    [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35],
+    [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
+  ];
+  
+  redNumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
 
   getNumberClass(num: number): string {
     return num < 10 ? 'single' : 'double';
@@ -59,5 +70,15 @@ export class RouletteComponent {
     }, this.spinDuration);
   }
 
-}
+  
+  isRed(num: number): boolean {
+    return this.redNumbers.includes(num);
+  }
+  
+  isBlack(num: number): boolean {
+    return num !== 0 && !this.redNumbers.includes(num);
+  }
+  
+  calculateWinnings(winningNumber: number): void {}
 
+}
