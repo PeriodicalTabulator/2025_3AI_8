151571fdbd70141f98b05b74568b3bb1765df18c
BacmanXD <samuelbacka71@gamil.com>
2025-04-16 07:52:44 +0200

    animation

--- luckyReels/src/menu/menu.component.ts
+++ luckyReels/src/menu/menu.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports:[MatSidenavModule, MatButtonModule],
+  imports: [MatSidenavModule, MatButtonModule],
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  animations: [
+    trigger('flyInFromTop', [
+      transition(':enter', [
+        style({ transform: 'translateY(-100%)', opacity: 0 }),
+        animate('600ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class MenuComponent {
   constructor(private router: Router) {}
-  
+
   navigateToGame(route: string) {
     this.router.navigate([route]);
   }
